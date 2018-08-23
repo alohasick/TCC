ded87f1030ab3e74770aee1c78bc866363691493
Israel Siqueira <jrmeds@gmail.com>
2018-08-23 11:23:28 -0300

    update do index.py

--- index.py
+++ index.py
@@ -1,1 +1,122 @@
-print('Hello World')
+import matplotlib.pyplot as plt
+import re
+import math
+import shapely.geometry as shapgeo
+
+#METODOS
+
+def getDistancias (locais, terreno) :
+    dist = 0
+    rota = False
+    distancias = dict()
+    for k1 in locais:
+        x1, y1 = locais[k1]
+        for k2 in locais:
+            if k1 != k2 : #teste pra nao calcular distancia tipo AA
+                if (k2 + k1) not in distancias : # dist AB = dist BA logo pula a iteração
+                    if isRota(locais, terreno, rota, k1, k2): #testar possibilidade de rota
+                        x2, y2 = locais[k2]
+                        dist = math.sqrt((x1-x2)**2 + (y1-y2)**2)
+                        distancias[k1 + k2] = dist
+                    else:
+                        x2, y2 = locais[k2]
+                        dist = math.sqrt((x1-x2)**2 + (y1-y2)**2)
+                        distancias[k1 + k2] = dist*(10**6) #caso a rota seja False multiplicará por um fator alto
+                else:
+                    pass
+    return distancias
+
+def setPontos (fname) :
+    pontos = dict()
+    fhand = open(fname) #ler arquivo
+    for line in fhand: 
+        line = line.rstrip()
+        chave = re.findall('^[(A-Za-z)]', line) #get key
+        x = re.findall('([0-9]+),', line) #get a from a, b
+        y = re.findall(',([0-9]+)', line) #get b from a, b
+        pontos[chave[0]] = float(x[0]), float(y[0]) #atribui novo par {key: a, b} ao dicio pontos
+    return pontos
+
+def getPontos (dicio):
+    x = list()
+    y = list()
+    for value in dicio : #coleta o valor das coordenadas em listas
+        coordx, coordy = dicio[value]
+        x.append(coordx) #dimensao x
+        y.append(coordy) #dimensao y
+    return x, y
+
+def isRota (locais, terreno, rota, k1, k2) :
+    vertices = list()
+    for ponto in terreno: vertices.append(terreno[ponto])
+    area = shapgeo.LinearRing(vertices) #define o poligono do terreno
+    reta = shapgeo.LineString([locais[k1], locais[k2]]) #define reta entre os pontos
+    #print(reta)
+    rota = not(reta.intersects(area)) #checa intersecçao, TRUE se nao houver intersecçao
+    return rota
+
+def writeCusto (locais, distancia):
+    file = open('custos.txt', 'w')
+    linhat, cabecalho, linha, pontos  = '', '', '', ''
+    linha = 'data; \nparam n :=8; #numero de cidades \n\n'
+    file.write(linha)
+    for ponto in locais:
+        pontos = pontos + ' ' + ponto
+    linha = 'set I:=' + pontos + '; #cidades de \nset J:=' + pontos + '; #cidades para \n\n#Matriz de Custos:\n'
+    file.write(linha)
+    cabecalho = 'param C: \t'
+    for local in locais:
+        cabecalho = cabecalho + '     ' + local
+    file.write(cabecalho + ':=')
+    for k1 in locais:
+        linhat = ''
+        linhat = '\n\t\t\t' + linhat + k1 + '  '
+        for k2 in locais:
+            if k1 != k2:
+                if (k2 + k1) not in distancia :
+                    linhat = linhat + ' ' + str(round(distancia[k1+k2], 2)) + ' '
+                else:
+                    linhat = linhat + ' ' + str(round(distancia[k2+k1], 2)) + ' '
+            else:
+                linhat = linhat + ' - '
+        linhat = linhat
+        file.write(linhat + " \r")
+    file.write('end;')
+    file.close()
+    return file
+#MAIN
+
+#leitura e extracao das coordenadas
+#NomeLocais = input('insira nome do arquivo dos pontos: ')
+locais = setPontos('pontos.txt') #coleta as coordenadas dos locais
+#print (locais) 
+
+#NomeTerreno = input('insira nome do arquivo do terreno: ')
+terreno = setPontos('terreno.txt')
+#print (terreno)
+
+distancias = getDistancias(locais, terreno) #determina distancias entre os pontos
+print(distancias)
+
+#plotar pontos e terreno
+x1, y1 = getPontos(locais)
+fig = plt.figure(dpi=200) #construcao do objeto grafico
+ax = fig.add_axes([0.13, 0.13, 0.83, 0.83])
+ax.scatter(x1, y1, color='red', marker='.')
+pontosLocais = locais.keys()
+for i, point in enumerate(pontosLocais): #nomeamento dos pontos
+    ax.annotate('\n ' + point, (x1[i], y1[i]))
+
+x2, y2 = getPontos(terreno)
+x2.append(x2[0])
+y2.append(y2[0])
+ax.plot(x2, y2, color='black', marker='.')
+pontosTerreno = terreno.keys()
+for i, point in enumerate(pontosTerreno): #nomeamento dos pontos
+    ax.annotate('\n ' + point, (x2[i], y2[i]))
+
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+#plt.show()
+
+writeCusto(locais, distancias)
